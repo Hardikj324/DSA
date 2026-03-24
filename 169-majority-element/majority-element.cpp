76537a0c1190d54cea46b32aeb4b71f50c1ba95c
Hardikj324 <157798760+Hardikj324@users.noreply.github.com>
2026-03-24 19:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (86.77%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+            int count = 0;
+    int element;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (count == 0)
+        {
+            element = arr[i];
+            count++;
+        }
+        else if (arr[i] == element)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+    }
+    return element;
+    }
+};
