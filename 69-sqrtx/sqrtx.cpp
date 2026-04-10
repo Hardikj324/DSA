f383cbc3393630e6e1aeb381f2c5c8dd28e22f35
Hardikj324 <157798760+Hardikj324@users.noreply.github.com>
2026-04-11 00:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.55%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        int left = 1;
-        int right = x / 2;
-        long long mid;
-        int ans = 0;
-        if(x<2){return x;}
-        while (left <= right)
+    int mySqrt(int x) 
+    {
+        int ans;
+        long long right = x,left = 0,mid;
+        while(left <= right)
         {
-            mid = (left + right) / 2;
-            cout << "This is the value of right " << right << endl;
-            cout << "This is the value of mid " << mid << endl;
-            cout << "This is the value of left " << left << endl;
-
-            if (mid * mid <= x)
+            mid = (left + right)/2;
+            if(mid * mid == x)
             {
-                ans = mid; // This line is because if we got this line so we will have the previous rounded of answer of the x.
+                return mid;
+            }
+            else if((mid * mid) < x)
+            {
+                ans = mid;
                 left = mid + 1;
             }
             else
