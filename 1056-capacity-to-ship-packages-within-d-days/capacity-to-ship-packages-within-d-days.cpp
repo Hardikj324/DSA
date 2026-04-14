c6949de37b306a2ac4c0fd4ea88fff436f2df0c7
Hardikj324 <157798760+Hardikj324@users.noreply.github.com>
2026-04-14 09:00:01 +0530

    Time: 3 ms (99.24%) | Memory: 35.1 MB (53.17%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,41 +1,39 @@
-class Solution
-{
-public:
-    int daysNeeded(vector<int> arr, int w)
-    {
-        int needed_days = 1;
-        int currentLoad = 0;
-        for (int weight : arr)
-        {
-            if ((currentLoad + weight) > w)
-            {
-                needed_days++;
-                currentLoad = weight;
-            }
-            else
-            {
-                currentLoad += weight;
-            }
-        }
-        return needed_days;
-    }
-    int shipWithinDays(vector<int> &weights, int d)
-    {
-        int l = *max_element(weights.begin(), weights.end());
-        int r = accumulate(weights.begin(), weights.end(), 0);
+class Solution {
 
-        while (l <= r)
-        {
-            int m = (l + r) / 2;
-            if (daysNeeded(weights, m) > d)
-            {
-                l = m + 1;
+    int finddays(vector<int>& weights, int capacity){
+        int days = 1;
+        int currentLoad = 0;
+
+        for(auto w : weights){
+            if(currentLoad + w > capacity){
+                days++;
+                currentLoad = w;
             }
-            else
-            {
-                r = m - 1;
+            else {
+                currentLoad += w;
             }
         }
-        return l;
+        return days;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+
+        int low = *max_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
+
+        while(low < high){
+            int mid = low + (high - low)/2;
+
+            int needed = finddays(weights,mid);
+
+            if(needed <= days){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 };
