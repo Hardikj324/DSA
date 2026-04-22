e3b921d0593dff8f18ce9e44b065a2cb0dbe9009
Hardikj324 <157798760+Hardikj324@users.noreply.github.com>
2026-04-22 14:15:23 +0530

    Time: 41 ms (93.36%) | Memory: 18.6 MB (92.51%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -5,31 +5,23 @@ public:
     {
         int m = matrix.size();
         int n = matrix[0].size();
-        int high = m - 1;
+        int row = m - 1;
+        int col = 0;
 
-        while (high >= 0)
+        while (col < n && row >= 0)
         {
-            if (matrix[high][0] <= target && matrix[high][n - 1] >= target)
+            if (matrix[row][col] == target)
             {
-                int low = 0, second_high = n - 1;
-                while (low <= second_high)
-                {
-                    int mid = (low + second_high) / 2;
-                    if (matrix[high][mid] == target)
-                    {
-                        return true;
-                    }
-                    else if (matrix[high][mid] > target)
-                    {
-                        second_high = mid - 1;
-                    }
-                    else
-                    {
-                        low = mid + 1;
-                    }
-                }
+                return true;
             }
-            high--;
+            else if (matrix[row][col] > target)
+            {
+                row--;
+            }
+            else
+            {
+                col++;
+            }
         }
 
         return false;
