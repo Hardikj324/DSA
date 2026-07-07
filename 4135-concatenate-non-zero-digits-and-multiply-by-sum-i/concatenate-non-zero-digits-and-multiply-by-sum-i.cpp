683f6e3a1efc1c291ed8963e483f54233bea97f9
Hardikj324 <157798760+Hardikj324@users.noreply.github.com>
2026-07-07 16:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (89.09%) - LeetSync

---
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long x = 0;
+        long long sum = 0;
+        long long place = 1;
+
+        while (n > 0) {
+        int digit = n % 10;
+        if (digit != 0) {
+            x += 1LL* digit * place;
+            place *= 10;
+            sum+=digit;
+        }
+        n /= 10;
+    }
+
+        return x*sum;
+    }
+};
