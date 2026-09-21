5a916beec22f970902b2c6d232fece5925e965d3
Hardikj324 <157798760+Hardikj324@users.noreply.github.com>
2026-09-21 23:53:28 +0530

    Time: 47 ms (62.64%) | Memory: 29.2 MB (45.73%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-            sort(nums.begin(), nums.end());
-    vector<vector<int>> res;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> vec;
+        for(int i=0;i<n;i++){
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int sum;
+            int k =n-1;
+            int j = i+1;
+            while(j<k){
+                sum = nums[k] + nums[j] + nums[i];
+                if(sum>0){
+                    sum -=nums[k];
+                    k--;
+                }
+                else if(sum<0){
+                    sum -=nums[j];
+                    j++;
+                }
+                else {
+                    vec.push_back({nums[i],nums[j],nums[k]});
 
-    for (int i = 0; i < nums.size(); i++) {
-        // Skip duplicate values for i
-        if (i > 0 && nums[i] == nums[i-1]) continue;
+                    while( j<k && nums[j]==nums[j+1]) j++;
+                    while( j<k && nums[k]==nums[k-1]) k--;
 
-        int left = i + 1, right = nums.size() - 1;
-        while (left < right) {
-            int total = nums[i] + nums[left] + nums[right];
-
-            if (total < 0) {
-                left++;
-            } else if (total > 0) {
-                right--;
-            } else {
-                res.push_back({nums[i], nums[left], nums[right]});
-
-                // Skip duplicates for left
-                while (left < right && nums[left] == nums[left+1]) left++;
-                // Skip duplicates for right
-                while (left < right && nums[right] == nums[right-1]) right--;
-
-                left++;
-                right--;
+                    j++;
+                    k--;
+                    
+                }
             }
-        }
-    }
-    return res;
+        } 
 
+        return vec;
     }
 };
